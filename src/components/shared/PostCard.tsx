f304121aab08d120e7695710dd5937888c7a1d7c
Daria Hannochenko <hannochenko.pn@ucu.edu.ua>
2024-03-17 19:05:53 +0200

    Implemented post with our view template

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -15,9 +15,10 @@ const PostCard = ({ post }: PostCardProps) => {
   if (!post.creator) return;
 
   return (
-    <div className="post-card">
-      <div className="flex-between">
-        <div className="flex items-center gap-3">
+    <div className="post">
+      <div className="postWrapper">
+      <div className="postTop">
+        <div className="postTopLeft">
           <Link to={`/profile/${post.creator.$id}`}>
             <img
               src={
@@ -25,57 +26,34 @@ const PostCard = ({ post }: PostCardProps) => {
                 "/assets/icons/profile-placeholder.svg"
               }
               alt="creator"
-              className="w-12 lg:h-12 rounded-full"
+              className="postProfileImg"
             />
           </Link>
-
-          <div className="flex flex-col">
-            <p className="base-medium lg:body-bold text-dark-1">
-              {post.creator.name}
-            </p>
-            <div className="flex-center gap-2 text-light-3">
-              <p className="subtle-semibold lg:small-regular ">
-                {multiFormatDateString(post.$createdAt)}
-              </p>
-              •
-              {/* <p className="subtle-semibold lg:small-regular">
-                {post.location}
-              </p> */}
-            </div>
-          </div>
+          <span className="postUsername">{post.creator.name}</span>
+          <span className="postDate">{multiFormatDateString(post.$createdAt)}</span>
         </div>
-
-        <Link
-          to={`/update-post/${post.$id}`}
-          className={`${user.id !== post.creator.$id && "hidden"}`}>
-          <img
-            src={"/assets/icons/edit.svg"}
-            alt="edit"
-            width={20}
-            height={20}
-          />
-        </Link>
+        <div className="postTopRight">
+          <Link
+            to={`/update-post/${post.$id}`}
+            className={`${user.id !== post.creator.$id && "hidden"}`}>
+            <img
+              src={"/assets/icons/edit.svg"}
+              alt="edit"
+              width={20}
+              height={20}
+            />
+          </Link>
+        </div>
+        
       </div>
-
       <Link to={`/posts/${post.$id}`}>
-        <div className="small-medium lg:base-medium py-5  text-dark-1">
-          <p>{post.caption}</p>
-          <ul className="flex gap-1 mt-2">
-            {post.tags.map((tag: string, index: string) => (
-              <li key={`${tag}${index}`} className="text-light-3 small-regular">
-                {/* #{tag} */}
-              </li>
-            ))}
-          </ul>
-        </div>
+        <div className="postCenter" >
+                <span className="postText">{post.caption}</span>
+                {/* <img className="postImg" src="" alt="" /> */}
+                <img src={post.imageUrl || "/assets/icons/profile-placeholder.svg"} alt="post image" className="post-card_img"/>
 
-        <img
-          src={post.imageUrl || "/assets/icons/profile-placeholder.svg"}
-          alt="post image"
-          className="post-card_img"
-        />
+          </div>
       </Link>
-      
       <div className="postBottom">
               <div className="postBottomLeft">
                 <PostStats post={post} userId={user.id} />
@@ -85,11 +63,107 @@ const PostCard = ({ post }: PostCardProps) => {
               </div> 
       </div>
       
-      {/* <div className="postBottomRight"><Link to="/comments"><span className="postCommentText">comments</span></Link></div> */}
-      
-      
+    </div>
+            
     </div>
   );
 };
 
 export default PostCard;
+
+
+// import { Models } from "appwrite";
+// import { Link } from "react-router-dom";
+// import { PostStats } from "@/components/shared";
+// import { multiFormatDateString } from "@/lib/utils";
+// import { useUserContext } from "@/context/AuthContext";
+// import "./post.css";
+
+// type PostCardProps = {
+//   post: Models.Document;
+// };
+
+// const PostCard = ({ post }: PostCardProps) => {
+//   const { user } = useUserContext();
+
+//   if (!post.creator) return;
+
+//   return (
+//     <div className="post-card">
+//       <div className="flex-between">
+//         <div className="flex items-center gap-3">
+//           <Link to={`/profile/${post.creator.$id}`}>
+//             <img
+//               src={
+//                 post.creator?.imageUrl ||
+//                 "/assets/icons/profile-placeholder.svg"
+//               }
+//               alt="creator"
+//               className="w-12 lg:h-12 rounded-full"
+//             />
+//           </Link>
+
+//           <div className="flex flex-col">
+//             <p className="base-medium lg:body-bold text-dark-1">
+//               {post.creator.name}
+//             </p>
+//             <div className="flex-center gap-2 text-light-3">
+//               <p className="subtle-semibold lg:small-regular ">
+//                 {multiFormatDateString(post.$createdAt)}
+//               </p>
+//               •
+//               {/* <p className="subtle-semibold lg:small-regular">
+//                 {post.location}
+//               </p> */}
+//             </div>
+//           </div>
+//         </div>
+
+//         <Link
+//           to={`/update-post/${post.$id}`}
+//           className={`${user.id !== post.creator.$id && "hidden"}`}>
+//           <img
+//             src={"/assets/icons/edit.svg"}
+//             alt="edit"
+//             width={20}
+//             height={20}
+//           />
+//         </Link>
+//       </div>
+
+//       <Link to={`/posts/${post.$id}`}>
+//         <div className="small-medium lg:base-medium py-5  text-dark-1">
+//           <p>{post.caption}</p>
+//           <ul className="flex gap-1 mt-2">
+//             {post.tags.map((tag: string, index: string) => (
+//               <li key={`${tag}${index}`} className="text-light-3 small-regular">
+//                 {/* #{tag} */}
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+
+//         <img
+//           src={post.imageUrl || "/assets/icons/profile-placeholder.svg"}
+//           alt="post image"
+//           className="post-card_img"
+//         />
+//       </Link>
+      
+//       <div className="postBottom">
+//               <div className="postBottomLeft">
+//                 <PostStats post={post} userId={user.id} />
+//               </div>
+//               <div className="postBottomRight">
+//                 <Link to="/comments"><span className="postCommentText"> comments</span></Link>
+//               </div> 
+//       </div>
+      
+//       {/* <div className="postBottomRight"><Link to="/comments"><span className="postCommentText">comments</span></Link></div> */}
+      
+      
+//     </div>
+//   );
+// };
+
+// export default PostCard;
